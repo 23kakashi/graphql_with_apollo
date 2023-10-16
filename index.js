c34c40bff39dbebd6737d9777a4cb232017838d6
tapish sharma <tapish-sharma@pluralsight.com>
2023-10-16 12:39:53 +0530

    update apollo-sever with datasource

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
-const sessions = require('./data/sessions.json');
+const SessionAPI = require('./datasources/sessions');
 
 const typeDefs = gql`
   type Query {
@@ -16,6 +16,9 @@ const typeDefs = gql`
     date: String
     format: String
     track: String
+      @deprecated(
+        reason: "Too many sessions do not fit into a single track, we will be migrating to a tag based system in the future"
+      )
     level: String
   }
 `;
@@ -28,7 +31,11 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const dataSources = () => {
+  sessionAPI: new SessionAPI();
+};
+
+const server = new ApolloServer({ typeDefs, resolvers, dataSources });
 
 server.listen({ port: process.env.port || 4000 }).then(({ url }) => {
   console.log(`graphql running at ${url}`);
